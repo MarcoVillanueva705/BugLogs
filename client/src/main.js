f31a3e70ac07c0d603f1a22b45341f2baba28185
marco villanueva <villanuevamarco98@gmail.com>
2020-03-18 14:41:01 -0600

    added auth service init to main js

--- client/src/main.js
+++ client/src/main.js
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import AuthService from "./AuthService"
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
+async function init() {
+
+  let user = await AuthService.Authenticate()
+
+  if (user) { store.commit("setUser", user) }
+
+  else { router.push({ name: 'login' }) }
 new Vue({
   router,
   store,
   render: function (h) { return h(App) }
 }).$mount('#app')
+}
+init()
+
